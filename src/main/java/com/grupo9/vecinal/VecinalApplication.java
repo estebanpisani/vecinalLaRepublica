8e6855d1d9f2d7d4c04453a037aef9e96484c758
Adelmar Galetto <adelmargaletto@gmail.com>
2021-10-26 21:22:46 -0300

    primer commit

---
+++ src/main/java/com/grupo9/vecinal/VecinalApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.vecinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VecinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VecinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo9/vecinal/VecinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.vecinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
