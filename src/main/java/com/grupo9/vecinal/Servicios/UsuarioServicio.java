2b9fa65145fddf9526485f8bac241c20cb0ed03d
Adelmar Galetto <adelmargaletto@gmail.com>
2021-10-28 19:20:12 -0300

    usuarios casi terminado

--- src/main/java/com/grupo9/vecinal/Servicios/UsuarioServicio.java
+++ src/main/java/com/grupo9/vecinal/Servicios/UsuarioServicio.java
@@ -1,9 +1,13 @@
 package com.grupo9.vecinal.Servicios;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.grupo9.vecinal.Entidades.Actividad;
 import com.grupo9.vecinal.Entidades.Usuario;
 import com.grupo9.vecinal.Repositorios.UsuarioRepositorio;
 
@@ -34,6 +38,157 @@ public class UsuarioServicio {
 
 		} catch (Exception e) {
 			e.getMessage();
+		}
+
+	}
+
+	@Transactional
+	public void modificarUsuario(String nombreUsuario, String contrasenia, String contrasenia2, String emailUsuario,
+			String nombre, String apellido, Integer telefono, Integer id) throws Exception {
+		try {
+			validarDatosUsuario(nombreUsuario, contrasenia, contrasenia2, emailUsuario, nombre, apellido);
+			Optional<Usuario> respuesta = usuarioRepo.findById(id);
+
+			if (respuesta.isPresent()) {
+				Usuario usuario = respuesta.get();
+				usuario.setNombreUsuario(nombreUsuario);
+				usuario.setContrasenia(contrasenia);
+				usuario.setEmailUsuario(emailUsuario);
+				usuario.setNombre(nombre);
+				usuario.setApellido(apellido);
+				usuario.setTelefono(telefono);
+
+				usuarioRepo.save(usuario);
+
+			} else {
+				throw new Exception("Usuario no encontrado");
+			}
+
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+	}
+
+	@Transactional
+	public void bajaUsuario(Integer id) throws Exception {
+		try {
+			Optional<Usuario> respuesta = usuarioRepo.findById(id);
+
+			if (respuesta.isPresent()) {
+				Usuario usuario = respuesta.get();
+				usuario.setAlta(false);
+
+				usuarioRepo.save(usuario);
+
+			} else {
+				throw new Exception("Usuario no encontrado");
+			}
+
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuarios() throws Exception {
+		try {
+			return usuarioRepo.findAll();
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public Usuario buscarUsuario(Integer id) throws Exception {
+		try {
+			Optional<Usuario> respuesta = usuarioRepo.findById(id);
+
+			if (respuesta.isPresent()) {
+				Usuario usuario = respuesta.get();
+				return usuario;
+			} else {
+				throw new Exception("Usuario no encontrado");
+			}
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Actividad> mostrarActividadesUsuario(Integer id) throws Exception {
+		try {
+			Usuario usuario = buscarUsuario(id);
+			List<Actividad> actividades = usuario.getActividades();
+
+			if (actividades.isEmpty() || actividades == null) {
+				throw new Exception("No se encontraron actividades para el afiliado " + usuario.getNombre() + " "
+						+ usuario.getApellido());
+			} else {
+				return actividades;
+			}
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuariosAlta() throws Exception {
+		try {
+			return usuarioRepo.usuariosAlta();
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuariosBaja() throws Exception {
+		try {
+			return usuarioRepo.usuariosBaja();
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuariosCuotaAlDia() throws Exception {
+		try {
+			return usuarioRepo.usuariosAlDia();
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuariosDeudores() throws Exception {
+		try {
+			return usuarioRepo.usuariosAdeudada();
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Usuario> mostrarUsuariosApellido(String apellido) throws Exception {
+		try {
+			return usuarioRepo.usuariosApellido(apellido);
+
+		} catch (Exception e) {
+			throw new Exception("No se encontraron afiliados");
 		}
 
 	}
