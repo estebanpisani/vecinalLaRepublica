2cff7e91f601159d79f6197cc3e82bd08c377e3a
Adelmar Galetto <adelmargaletto@gmail.com>
2021-10-29 17:41:54 -0300

    Novedad 1

--- src/main/java/com/grupo9/vecinal/Repositorios/NovedadRepositorio.java
+++ src/main/java/com/grupo9/vecinal/Repositorios/NovedadRepositorio.java
@@ -1,6 +1,9 @@
 package com.grupo9.vecinal.Repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.grupo9.vecinal.Entidades.Novedad;
@@ -8,4 +11,9 @@ import com.grupo9.vecinal.Entidades.Novedad;
 @Repository
 public interface NovedadRepositorio extends JpaRepository<Novedad, Integer> {
 
+	@Query("SELECT n FROM Novedad n WHERE n.alta = true ORDER BY n.fecha ASC")
+	public List<Novedad> novedadesAlta();
+	
+	@Query("SELECT n FROM Novedad n WHERE n.alta = false ORDER BY n.fecha ASC")
+	public List<Novedad> novedadesBaja();
 }
--- src/main/java/com/grupo9/vecinal/Servicios/NovedadServicio.java
+++ src/main/java/com/grupo9/vecinal/Servicios/NovedadServicio.java
@@ -1,6 +1,8 @@
 package com.grupo9.vecinal.Servicios;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,23 +18,100 @@ public class NovedadServicio {
 	NovedadRepositorio novedadRepo;
 
 	@Transactional
-	public void crearNovedad(String titulo, String descripcion) {
-		
+	public void crearNovedad(String titulo, String descripcion, Boolean destacado) {
+
 		try {
 			validarDatosNovedad(titulo, descripcion);
-			Novedad novedad =new Novedad();
+			Novedad novedad = new Novedad();
 			novedad.setDescripcion(descripcion);
 			novedad.setTitulo(titulo);
 			novedad.setAlta(true);
 			novedad.setFecha(new Date());
+			novedad.setDestacado(destacado);
+
+			novedadRepo.save(novedad);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
+	@Transactional
+	public void modificarNovedad(String titulo, String descripcion, Boolean destacado, Integer id) throws Exception {
+
+		try {
+			validarDatosNovedad(titulo, descripcion);
+			Optional<Novedad> respuesta = novedadRepo.findById(id);
+			if (respuesta.isPresent()) {
+				Novedad novedad = respuesta.get();
+				novedad.setDescripcion(descripcion);
+				novedad.setTitulo(titulo);
+				novedad.setFecha(new Date());
+				novedad.setDestacado(destacado);
+
+				novedadRepo.save(novedad);
+			} else {
+				throw new Exception("La noticia no fue modificada");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Transactional
+	public void bajaNovedad(Integer id) throws Exception {
+
+		try {
+			Optional<Novedad> respuesta = novedadRepo.findById(id);
+			if (respuesta.isPresent()) {
+				Novedad novedad = respuesta.get();
+				novedad.setAlta(false);
+
+				novedadRepo.save(novedad);
+			} else {
+				throw new Exception("Error al dar de baja a la noticia");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<Novedad> mostrarTodasNovedades() throws Exception {
+		List<Novedad> novedades = novedadRepo.findAll();
+		if (novedades.isEmpty()) {
+			throw new Exception("No hay noticias para mostrar");
+		} else {
+			return novedades;
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Novedad> mostrarAltaNovedades() throws Exception {
+		List<Novedad> novedades = novedadRepo.novedadesAlta();
+		if (novedades.isEmpty()) {
+			throw new Exception("No hay noticias activas");
+		} else {
+			return novedades;
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Novedad> mostrarBajaNovedades() throws Exception {
+		List<Novedad> novedades = novedadRepo.novedadesAlta();
+		if (novedades.isEmpty()) {
+			throw new Exception("No hay noticias dadas de baja");
+		} else {
+			return novedades;
+		}
+	}
+
 	public void validarDatosNovedad(String titulo, String descripcion) throws Exception {
-		
+
 		if (titulo == null || titulo.isEmpty())
 
 		{
